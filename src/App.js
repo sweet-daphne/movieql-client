58e637d13d204b022bb616ecff192b4a229f41b1
suelrin <dancerelrin@gmail.com>
2023-03-21 07:54:44 +0900

    setting basic

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movies from "./routes/Movies";
+import Movie from "./routes/Movie";
+
+export default function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Movies />} />
+      <Route path="/movies/:id" element={<Movie />} />
+    </Routes>
+  </BrowserRouter>;
+}
