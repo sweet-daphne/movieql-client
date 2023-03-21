85dd01223ce0358b23a9adccd3bb99c6355e2b14
suelrin <dancerelrin@gmail.com>
2023-03-22 07:50:08 +0900

    apolloclient setting & provider

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,25 @@
-export default function Movies(){
-  return <div>This is a list of movies.</div>;
+import { useApolloClient, gql } from "@apollo/client";
+import { useEffect, useState } from "react";
+
+export default function Movie(){
+  const [movies, setMovies] = useState([]);
+  const client = useApolloClient();
+  useEffect(() => {
+    client.query({
+      query: gql`
+        {
+          allMovies{
+            title
+          }
+        }
+      `
+    }).then((result) => setMovies(result.data.allMovies));
+  },[client]);
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  );
 }
